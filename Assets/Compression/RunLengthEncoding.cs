072c1c8eeb4d0379dc040494d665ca3dcff1146c
MORAI-mklee <mklee@morai.ai>
2022-11-18 20:45:45 +0900

    Thread 적용

--- Assets/Compression/RunLengthEncoding.cs
+++ Assets/Compression/RunLengthEncoding.cs
@@ -13,72 +13,59 @@ count : 복사할 byte 수
 
 */
 
-public class RunLengthEncoding 
+public class RunLengthEncoding
 {
     public byte[] m_firstPix = new byte[3];
     public byte[] m_secondPix = new byte[3];
-    public byte[] m_outputByte = new byte[5];
+    public byte[] s_countByte = new byte[2];
+    byte[] m_outputByte = new byte[921600]; // 640*480*3
     public ushort count = 1;
     public int second_count = 0;
     public int m_loopCounter = 0;
 
     public byte[] Encode(byte[] data) 
     {
+        
         var f_countByte = BitConverter.GetBytes(count);
-        f_countByte.CopyTo(m_outputByte, 0);
+        Buffer.BlockCopy(f_countByte, 0, m_outputByte, 0, sizeof(ushort));
+        Buffer.BlockCopy(data, 0, m_outputByte, 2, 3);
 
-        m_firstPix[0] = data[0];
-        m_firstPix[1] = data[1];
-        m_firstPix[2] = data[2];
-
-        m_firstPix.CopyTo(m_outputByte, 2);
-
-        for (int i = 0; 3 * i < data.Length - 3; i++)
+        for (int i = 0; i < data.Length/3 - 1; i++)
         {
+            //m_firstPix[0] = data[3 * i];
+            //m_firstPix[1] = data[3 * i + 1];
+            //m_firstPix[2] = data[3 * i + 2];
 
-            m_firstPix[0] = data[3 * i];
-            m_firstPix[1] = data[3 * i + 1];
-            m_firstPix[2] = data[3 * i + 2];
+            //m_secondPix[0] = data[3 * (i + 1)];
+            //m_secondPix[1] = data[3 * (i + 1) + 1];
+            //m_secondPix[2] = data[3 * (i + 1) + 2];
 
-            m_secondPix[0] = data[3 * (i + 1)];
-            m_secondPix[1] = data[3 * (i + 1) + 1];
-            m_secondPix[2] = data[3 * (i + 1) + 2];
-
-            if (m_firstPix[0] == m_secondPix[0] && m_firstPix[1] == m_secondPix[1] && m_firstPix[2] == m_secondPix[2])
+            if (data[3 * i] == data[3 * (i + 1)] && data[3 * i + 1] == data[3 * (i + 1) + 1] && data[3 * i + 2] == data[3 * (i + 1) + 2])
             {
                 count += 1;
-                
-                //just count increase
-                var s_countByte = BitConverter.GetBytes(count);
 
-                s_countByte.CopyTo(m_outputByte, second_count * (m_outputByte.Length-5));
+                s_countByte = BitConverter.GetBytes(count);
+                Buffer.BlockCopy(s_countByte, 0, m_outputByte, 0, sizeof(ushort));
+                //s_countByte.CopyTo(m_outputByte, m_outputByte.Length - 5);
 
             }
             else
             {
-                count = 1;
-                second_count += 1;
+                //count = 1;
 
-                var s_countByte = BitConverter.GetBytes(count);
+                //s_countByte = BitConverter.GetBytes(count);
 
-                //s_countByte.CopyTo(m_outputByte, second_count * m_outputByte.Length);
+                //m_outputByte = AddByteArrayToArray(m_outputByte, s_countByte);
 
-                m_outputByte = AddByteArrayToArray(m_outputByte, s_countByte);
-
-                m_outputByte = AddByteArrayToArray(m_outputByte, m_secondPix);
-
-                //m_outputByte = new byte[firstoutput.Length + m_secondPix.Length];
-
-                //firstoutput.CopyTo(m_outputByte, 0);
-                //m_secondPix.CopyTo(m_outputByte, firstoutput.Length);
+                //m_outputByte = AddByteArrayToArray(m_outputByte, m_secondPix);
 
             }
-
-            m_loopCounter += 1;
+     
 
         }
 
         return m_outputByte;
+
     }
 
     public byte[] AddByteArrayToArray(byte[] bArray, byte[] newByteArray)
@@ -96,5 +83,6 @@ public class RunLengthEncoding
         newArray[newArray.Length - 1] = newByte;
         return newArray;
     }
+
 
 }
--- Assets/Scripts/Image_Compression.cs
+++ Assets/Scripts/Image_Compression.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using System.Linq;
+using System.Threading;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 
 public class Image_Compression : MonoBehaviour
 {
     //Camera Resolution
     public int m_cameraWidth = 640;
     public int m_cameraHeight = 480;
-    public RunLengthEncoding RLE = new RunLengthEncoding();
+    CancellationTokenSource cancellationTokenSource;
 
     //Scene Camera를 가져오기 위한 멤머 변수
     private Camera m_Camera;
@@ -18,39 +24,237 @@ public class Image_Compression : MonoBehaviour
     Rect textureRect;
 
     //Camera의 RGB Data를 포함하고 있는 Byte buffer
-    byte[] CompressionOutput;
-    private float Compression_Ratio;
+
+    public byte[] m_firstPix = new byte[3];
+    public byte[] m_secondPix = new byte[3];
+    public byte[] s_countByte = new byte[2];
+    byte[] m_outputByte = new byte[5]; // 640*480*3
+    int m_loopCounter = 0;
+    int count = 0;
+    int count_Equal = 0;
+    int count_nEqual = 0;
+    public int second_count = 0;
+    Thread thread;
+    byte[] _rawTextureBuffer;
+    public Texture2D tex;
+    
+    public class ImageData
+    {
+        public byte[] imgBytes;
 
 
+        public ImageData(byte[] imgBytes)
+        {
+            this.imgBytes = imgBytes;
+
+        }
+    }
     void Start()
     {
+
         m_Camera = GetComponent<Camera>();
         m_Camera.renderingPath = RenderingPath.UsePlayerSettings;
 
-        colorBuffer = new Texture2D(m_cameraWidth, m_cameraHeight, TextureFormat.RGB24, false);
+        
         renderINIT = new RenderTexture(m_cameraWidth, m_cameraHeight, 24);
         renderINIT.filterMode = FilterMode.Point;
+        
 
         textureRect = new Rect(0, 0, m_cameraWidth, m_cameraHeight);
+
+        m_Camera.targetTexture = renderINIT;
+
+        _rawTextureBuffer = new byte[m_cameraWidth * m_cameraHeight * 3];
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_Camera.targetTexture = renderINIT;
-        RenderTexture.active = m_Camera.targetTexture;
-        colorBuffer.ReadPixels(textureRect, 0, 0);
-        colorBuffer.Apply();
+        tex = GetRTPixels(m_Camera.targetTexture);
+        
+        //var tex_byte = tex.GetRawTextureData();
+        //tex.ReadPixels(textureRect, 0, 0);
+        tex.Apply();
 
-        byte[] texture_byte = colorBuffer.GetRawTextureData();
+        var request = AsyncGPUReadback.Request(tex, 0, OnCompleteReadBack);
+        request.WaitForCompletion();
+        //Encode(tex_byte);
 
-        CompressionOutput = RLE.Encode(texture_byte);
+        //CopyNativeArrayToByteArray(ref req, ref _rawTextureBuffer);
+        UnityEngine.Debug.Log("Count_Equal : " + count_Equal);
+        UnityEngine.Debug.Log("Count_nEqual : " + count_nEqual);
+        UnityEngine.Debug.Log("m_firstPixLength : " + m_firstPix.Length);
+        UnityEngine.Debug.Log("m_outputByte : " + m_outputByte.Length);
+        //UnityEngine.Debug.Log(
+        UnityEngine.Debug.Log((float)(m_outputByte.Length) * 100.0f / (float)(640*480*3));
 
-        Compression_Ratio = ((float)CompressionOutput.Length / (float)texture_byte.Length) * 100.0f;
+        Destroy(tex);
 
-        UnityEngine.Debug.Log(Compression_Ratio);
+    }
+
+    private void OnDestroy()
+    {
+        AsyncGPUReadback.WaitAllRequests();
 
 
+        cancellationTokenSource.Dispose();
+        
+    }
+
+    Texture2D GetRTPixels(RenderTexture rt)
+    {
+        RenderTexture.active = rt;
+        Texture2D colorBuffer = new Texture2D(m_cameraWidth, m_cameraHeight, TextureFormat.RGB24, false);
+        colorBuffer.ReadPixels(new Rect(0, 0, m_cameraWidth, m_cameraHeight), 0, 0);
+        
+        RenderTexture.active = null;
+        return colorBuffer;
+    }
+
+    public void Encode(object obj)
+    {
+        m_outputByte = new byte[5];
+        m_firstPix = new byte[3];
+        m_secondPix = new byte[3];
+        ImageData data = (ImageData)obj;
+        m_loopCounter = 0;
+        count = 0;
+        count_Equal = 0;
+        count_nEqual = 0;
+
+
+        if(data.imgBytes != null)
+        {
+            for (int i = 0; i < (data.imgBytes.Length / 3) - 1; i++)
+            {
+                Buffer.BlockCopy(data.imgBytes, 3 * i, m_firstPix, 0, 3);
+                Buffer.BlockCopy(data.imgBytes, 3 * (i + 1) , m_secondPix, 0, 3);
+
+                //Debug.Log(data.imgBytes.Length);
+
+                if (m_firstPix.SequenceEqual(m_secondPix))
+                {
+                    count += 1;
+                    count_Equal += 1;
+                    var countByte = BitConverter.GetBytes(count);
+                    Buffer.BlockCopy(countByte, 0, m_outputByte, 8 * count_nEqual, sizeof(int));
+                }
+                else
+                {
+
+                    count_nEqual += 1;
+                    var countByte_nEqual = BitConverter.GetBytes(1);
+
+                    Buffer.BlockCopy(countByte_nEqual, 0, m_outputByte, 8 * count_nEqual, sizeof(int));
+                    //m_outputByte = AddByteArrayToArray(m_outputByte, countByte_nEqual);
+                    //Buffer.BlockCopy(m_secondPix, 0, m_outputByte, 8 * count_nEqual + 4, 3);
+                    //m_outputByte = AddByteArrayToArray(m_outputByte, m_secondPix);
+                }
+            }
+
+        }
+        
+    }
+
+    void OnCompleteReadBack(AsyncGPUReadbackRequest request)
+    {
+        if (thread != null)
+        {
+            thread.Abort();
+        }
+
+        thread = new Thread(Encode);
+        thread.IsBackground = true;
+        thread.Start(new ImageData(_rawTextureBuffer));
+        thread.Join();
+
+        var req = request.GetData<byte>();
+        CopyNativeArrayToByteArray(ref req, ref _rawTextureBuffer);
+
+
+    }
+
+    public static unsafe void CopyNativeArrayToByteArray<T>(ref NativeArray<T> src, ref byte[] dst) where T : unmanaged
+    {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+        AtomicSafetyHandle.CheckReadAndThrow(NativeArrayUnsafeUtility.GetAtomicSafetyHandle(src));
+        if (dst == null)
+            throw new ArgumentNullException(nameof(dst));
+#endif
+
+        var size = UnsafeUtility.SizeOf<T>() * src.Length;
+        if (dst.Length != size)
+            dst = new byte[size];
+
+        var srcAddr = (byte*)src.GetUnsafeReadOnlyPtr();
+
+        fixed (byte* dstAddr = dst)
+        {
+            UnsafeUtility.MemCpy(&dstAddr[0], &srcAddr[0], size);
+        }
+    }
+
+    public static unsafe void CopyNativeArrayToManagedArray<T>(ref NativeArray<T> src, ref T[] dst) where T : unmanaged
+    {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+        AtomicSafetyHandle.CheckReadAndThrow(NativeArrayUnsafeUtility.GetAtomicSafetyHandle(src));
+        if (dst == null)
+            throw new ArgumentNullException(nameof(dst));
+#endif
+
+        var size = src.Length;
+        if (size != dst.Length)
+        {
+            dst = new T[size];
+        }
+
+        var srcAddr = (T*)src.GetUnsafeReadOnlyPtr();
+
+        fixed (T* dstAddr = dst)
+        {
+            UnsafeUtility.MemCpy(&dstAddr[0], &srcAddr[0], size);
+        }
+    }
+
+    public static unsafe void CopyManagedArrayToNativeArray<T>(ref T[] src, ref NativeArray<T> dst) where T : unmanaged
+    {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+        AtomicSafetyHandle.CheckReadAndThrow(NativeArrayUnsafeUtility.GetAtomicSafetyHandle(dst));
+        if (src == null)
+            throw new ArgumentNullException(nameof(src));
+#endif
+
+        var size = src.Length;
+        if (size != dst.Length)
+        {
+            dst.Dispose();
+            dst = new NativeArray<T>(size, Allocator.Persistent);
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            AtomicSafetyHandle.CheckReadAndThrow(NativeArrayUnsafeUtility.GetAtomicSafetyHandle(dst));
+#endif
+        }
+
+        var dstAddr = (T*)dst.GetUnsafeReadOnlyPtr();
+        fixed (T* srcAddr = src)
+        {
+            UnsafeUtility.MemCpy(&dstAddr[0], &srcAddr[0], src.Length);
+        }
+    }
+
+    public static unsafe void ClearNativeArray<T>(NativeArray<T> src, int length) where T : unmanaged
+    {
+        UnsafeUtility.MemClear(
+            NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks(src),
+            UnsafeUtility.SizeOf<T>() * length);
+    }
+
+    public byte[] AddByteArrayToArray(byte[] bArray, byte[] newByteArray)
+    {
+        byte[] newArray = new byte[bArray.Length + newByteArray.Length];
+        bArray.CopyTo(newArray, 0);
+        newByteArray.CopyTo(newArray, bArray.Length);
+        return newArray;
     }
 
 }
